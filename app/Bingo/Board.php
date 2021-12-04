ac6ecc07bd49dc17b19143b195689bbca2e79538
AngryMoustache <sander.vandamme11@gmail.com>
2021-12-04 11:16:05 +0100

    Day 4

---
+++ app/Bingo/Board.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Bingo;
+
+class Board
+{
+    public $board;
+
+    protected $checks = [
+        // Horizontal
+        [0, 1, 2, 3, 4],
+        [5, 6, 7, 8, 9],
+        [10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19],
+        [20, 21, 22, 23, 24],
+
+        // Vertical
+        [0, 5, 10, 15, 20],
+        [1, 6, 11, 16, 21],
+        [2, 7, 12, 17, 22],
+        [3, 8, 13, 18, 23],
+        [4, 9, 14, 19, 24],
+
+        // Diagonal
+        // [0, 6, 15, 18, 24],
+        // [4, 8, 12, 16, 20],
+    ];
+
+    public function __construct($board)
+    {
+        $this->board = collect($board)->map(function ($row) {
+            return collect($row)->flip()->map(fn () => false);
+        });
+    }
+
+    public function mark($number)
+    {
+        $this->board = $this->board->map(function ($row) use ($number) {
+            return $row->map(function ($value, $key) use ($number) {
+                return ($key == $number) ? true : $value;
+            });
+        });
+    }
+
+    public function check()
+    {
+        $board = $this->board->flatten()->filter()->keys()->toArray();
+        foreach ($this->checks as $check) {
+            if (count(array_intersect($board, $check)) === 5) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function sum()
+    {
+        return $this->board->flatMap(function ($row) {
+            return $row->reject()->keys();
+        })->sum();
+    }
+}
---
+++ app/Http/Controllers/Day4Controller.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Bingo\Board;
+
+class Day4Controller extends Controller
+{
+    public $day = 4;
+
+    public $drawings;
+
+    public $boards;
+
+    public $winners;
+
+    public function parseData()
+    {
+        $this->winners = collect();
+        $this->drawings = collect($this->data['drawings']);
+        $this->boards = collect($this->data['boards'])
+            ->map(fn ($board) => new Board($board));
+    }
+
+    public function first()
+    {
+        // Draw a new number
+        $number = $this->drawings->shift();
+
+        // Mark it on each of the boards
+        $this->boards->each(fn ($board) => $board->mark($number));
+
+        // Check for bingo
+        $bingo = $this->boards->skipUntil(fn ($board) => $board->check());
+
+        // Continue if no bingo was found
+        if ($bingo->isEmpty() && $this->drawings->isNotEmpty()) {
+            return $this->first();
+        }
+
+        // Get final result
+        return $bingo->first()->sum() * $number;
+    }
+
+    public function second()
+    {
+        // Draw a new number
+        $number = $this->drawings->shift();
+
+        // Mark it on each of the boards
+        $this->boards->each(fn ($board) => $board->mark($number));
+
+        // Check for bingo & add to the winners
+        $this->boards = $this->boards->reject(function ($board) {
+            if ($board->check()) {
+                $this->winners->prepend($board);
+                return true;
+            }
+
+            return false;
+        });
+
+        // Continue until the end
+        if ($this->drawings->isNotEmpty() && $this->boards->isNotEmpty()) {
+            return $this->second();
+        }
+
+        // Get final result
+        return $this->winners->first()->sum() * $number;
+    }
+}
