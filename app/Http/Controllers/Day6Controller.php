6556545a35619ed794811209f71fb597efbd514f
AngryMoustache <sander.vandamme11@gmail.com>
2021-12-06 12:46:15 +0100

    Day 6 - Tweaks

--- app/Http/Controllers/Day6Controller.php
+++ app/Http/Controllers/Day6Controller.php
@@ -11,33 +11,27 @@ class Day6Controller extends Controller
     public function parseData()
     {
         $this->fish = collect()->pad(9, 0);
-        $this->data->each(function ($timer) {
-            $this->fish[$timer] += 1;
-        });
+        $this->data->each(fn ($timer) => $this->fish[$timer] += 1);
     }
 
     public function first()
     {
-        for ($day = 1; $day <= 80; $day++) {
-            $this->day();
-        }
-
-        return $this->fish->sum();
+        return $this->days(80);
     }
 
     public function second()
     {
-        for ($day = 1; $day <= 256; $day++) {
-            $this->day();
+        return $this->days(256);
+    }
+
+    public function days($amount)
+    {
+        for ($day = 1; $day <= $amount; $day++) {
+            $birthing = $this->fish->shift();
+            $this->fish[6] += $birthing;
+            $this->fish = $this->fish->concat([$birthing]);
         }
 
         return $this->fish->sum();
-    }
-
-    public function day()
-    {
-        $birthing = $this->fish->shift();
-        $this->fish[6] += $birthing;
-        $this->fish = $this->fish->concat([$birthing]);
     }
 }
