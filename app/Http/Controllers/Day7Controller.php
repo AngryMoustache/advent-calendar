d8678e8845bd6fe5f2dd9e7d35ef2f47ef6c433b
AngryMoustache <sander.vandamme11@gmail.com>
2021-12-07 13:55:51 +0100

    Cleaner day 7

--- app/Http/Controllers/Day7Controller.php
+++ app/Http/Controllers/Day7Controller.php
@@ -4,28 +4,24 @@ namespace App\Http\Controllers;
 
 class Day7Controller extends Controller
 {
-    public $fuel = 0;
+    public $fuel = [];
 
     public function first()
     {
         $goto = $this->data->median();
-
-        $this->data->each(function ($from) use ($goto) {
-            $this->fuel += abs($from - $goto);
-        });
-
-        return $this->fuel;
+        return $this->data->map(fn ($from) => abs($from - $goto))->sum();
     }
 
     public function second()
     {
-        $goto = $this->data->median();
+        for ($to = 0; $to < $this->data->max(); $to++) {
+            $this->fuel[$to] = 0;
+            $this->data->each(function ($from) use ($to) {
+                $steps = abs($from - $to);
+                $this->fuel[$to] += ($steps / 2) * ($steps + 1);
+            });
+        }
 
-        $this->data->each(function ($from) use ($goto) {
-            $steps = abs($from - $goto);
-            $this->fuel += ($steps / 2) * ($steps + 1);
-        });
-
-        return $this->fuel;
+        return min($this->fuel);
     }
 }
