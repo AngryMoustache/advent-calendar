8ba1a738ce7ad5837565ab0428753c2077d5905c
AngryMoustache <sander.vandamme11@gmail.com>
2021-12-15 16:04:50 +0100

    Day 15

---
+++ app/Http/Controllers/Day15Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Day15Controller extends Controller
+{
+    public function first()
+    {
+
+    }
+
+    public function second()
+    {
+
+    }
+}
